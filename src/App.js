0e91d3dc0692c2a106949f7de5360b8a74c20fc1
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-01-05 01:35:29 +0530

    fetch videos from api and add watch page for each video

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Body from "./components/Body";
-import Header from "./components/Header";
+
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
 import store from "./utils/store";
+import Header from "./components/Header";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
-      <Header />
-      <Body />
+      <div>
+        <Header />
+        <RouterProvider router={appRouter} />
+
+        {/**
+         *
+         * Head
+         * Body
+         *  Sidebar
+         *    MenuItems
+         *  MainContainer
+         *    ButtonsList
+         *    VideoContainer
+         *      VideoCard
+         *
+         *
+         */}
+      </div>
     </Provider>
   );
 }
---
+++ src/utils/contants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyBSwkE6vxfRBc3Ls_dogFupYcqg_v52RPg";
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyBSwkE6vxfRBc3Ls_dogFupYcqg_v52RPg&key=" +
+  GOOGLE_API_KEY;
